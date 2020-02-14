894c4cdff643f55c7774ce0fb494d7d1684164c5
nikulkafedor <veronikafed@mail.ru>
2020-02-14 11:59:47 +0300

    seminar

--- src/main/java/Serialization.java
+++ src/main/java/Serialization.java
@@ -1,14 +1,26 @@
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Serialization implements Serializable {
-    public static void writeObject(List<>){
-         try (FileWriter fileWriter = new FileWriter(Files.newInputStream(path))){
-             fileWriter.writeObject(animalList);
+
+     public static void serialize(List<Animal> listOfAnimals,Path path) throws IOException {
+         try (ObjectOutputStream outputStream =
+                      new ObjectOutputStream(Files.newOutputStream(path))) {
+             outputStream.writeObject(listOfAnimals);
          }
-     }
-     void readObject() {
 
      }
+    public static List<Animal> deserialize(Path path) throws IOException, ClassNotFoundException{
+         List<Animal> newAnimalList;
+        //Path path = Paths.get("object.bin");
+        try (ObjectInputStream inputStream =
+                     new ObjectInputStream(Files.newInputStream(path))) {
+            newAnimalList = (List<Animal>) inputStream.readObject();
+        }
+        return newAnimalList;
+    }
 
 }
---
+++ src/test/java/SerializationTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SerializationTest {
+
+    @Test
+    public void serialize() {
+
+    }
+
+    @Test
+    public void deserialize() {
+    }
+}
